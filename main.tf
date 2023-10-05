0148e9f5d11dfc8f7ac7a5c993996d73a6c49cfc
John Hackett <john@mutual.email>
2023-10-05 13:48:45 +0100

    create ecr and permit the caller (CI) to push docker images

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ terraform {
 }
 
 data "aws_availability_zones" "available" {}
+data "aws_caller_identity" "current" {}
 
 locals {
   region = "eu-west-2"
@@ -272,6 +273,34 @@ module "vpc" {
 
   enable_nat_gateway = true
   single_nat_gateway = true
+
+  tags = local.tags
+}
+
+module "ecr" {
+  source = "terraform-aws-modules/ecr/aws"
+
+  repository_name = "wagtail"
+  repository_type = "private"
+
+  repository_read_write_access_arns = [data.aws_caller_identity.current.arn]
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 30
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
 
   tags = local.tags
 }
