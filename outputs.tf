b8c9c8ea670e7a7e1a682e3de3e8df0a57e2ec33
John Hackett <john@mutual.email>
2023-10-17 12:16:57 +0100

    adding output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 output "postgres_password" {
-  value = jsondecode(data.aws_secretsmanager_secret_version.postgres_password.secret_string)
+  value     = jsondecode(data.aws_secretsmanager_secret_version.postgres_password.secret_string)
+  sensitive = true
+}
+
+output "application_password" {
+  value     = random_password.application_password.result
   sensitive = true
 }
