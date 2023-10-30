e08d817a71560453a1d781ddbed10834ef3e03d7
John Hackett <john.hackett@digital.cabinet-office.gov.uk>
2023-10-30 15:15:55 +0000

    adding log bucket

--- main.tf
+++ main.tf
@@ -288,7 +288,7 @@ module "alb" {
   ]
 
   access_logs = {
-    bucket = "${local.name}-logs"
+    bucket = module.log_bucket.s3_bucket_id
   }
 
   tags = local.tags
@@ -898,4 +898,25 @@ module "s3_bucket" {
       max_age_seconds = 3000
     }
   ]
+}
+
+module "log_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket_prefix = "${local.name}-logs-"
+  acl           = "log-delivery-write"
+
+  # For example only
+  force_destroy = true
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  attach_elb_log_delivery_policy = true # Required for ALB logs
+  attach_lb_log_delivery_policy  = true # Required for ALB/NLB logs
+
+  attach_deny_insecure_transport_policy = true
+  attach_require_latest_tls_policy      = true
+
+  tags = local.tags
 }
