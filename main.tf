7f8e9c0febee48ab4b3c426a257b00a8f8a03fc5
John Hackett <john.hackett@digital.cabinet-office.gov.uk>
2023-11-15 14:39:58 +0000

    s3 bucket config settings

--- main.tf
+++ main.tf
@@ -990,10 +990,10 @@ module "s3_bucket" {
 
   # S3 Bucket Ownership Controls
   # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_ownership_controls
-  control_object_ownership = false
-  #  object_ownership         = "BucketOwnerPreferred"
+  control_object_ownership = true
+  object_ownership         = "BucketOwnerPreferred"
 
-  #  expected_bucket_owner = aws_iam_user.wagtail.id
+  expected_bucket_owner = local.account_id
 
   #  acl = "private" # "acl" conflicts with "grant" and "owner"
 }
